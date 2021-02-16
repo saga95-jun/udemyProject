0cd510f2b427356a6997fd4bde2ec94d4a72b64d
Saga <example@gmail.com>
2021-02-16 22:09:30 +0300

    second task from udemy

--- js/script.js
+++ js/script.js
@@ -10,12 +10,28 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?',''),
-      b = prompt('На сколько его оцените?',''),
-      c = prompt('Один из последних просмотренных фильмов?',''),
-      d = prompt('На сколько его оцените?','');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let j = 0; j < 2; j++) {
+    const a = prompt('Один из последних просмотренных фильмов?',''),
+          b = prompt('На сколько его оцените?','');
+    
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+        j--;
+    }
+}
+
+if (personalMovieDB.count < 10 && personalMovieDB.count >= 0) {
+    console.log('Просмотрено мало фильмов!');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка!');
+}
+
+
 
 console.log(personalMovieDB);
